b03a40d5fb3e8918595677b14c3b6f07b43c6772
vishnu <vishnu981203@gmail.com>
2023-04-03 07:43:21 -0400

    Added tabs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "../src/index.css";
 import { useState } from "react";
 import { useEffect } from "react";
-
+import { v4 as uuidv4 } from "uuid";
+import "./index.css";
 const link = "https://course-api.com/react-tabs-project";
 
 const App = () => {
@@ -9,6 +9,8 @@ const App = () => {
   const [start, setStart] = useState(true);
   // data fetching state
   const [getData, setGetData] = useState([]);
+  // value state
+  const [value, setValue] = useState(0);
 
   // fetching data using async
   const fetchData = async () => {
@@ -26,23 +28,51 @@ const App = () => {
   }, []);
 
   if (start) {
-    <h2>Loading..!!</h2>;
+    return <div className="loading">Loading...</div>;
   }
+  // destructure
+  const { title, duties, dates } = getData[value];
   // component return
   return (
-    <>
-      <section className="section">
-        <div>
-          {getData.map((val, index) => {
+    <article className="container">
+      <div className="title">Tabs</div>
+      <div className="underline"></div>
+      <div></div>
+      <div className="title">
+        <h1>Tabs</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="btn-container">
+        {getData.map((val, index) => {
+          return (
+            <>
+              <button
+                key={index}
+                className={`job-btn   ${value === index ? "active-btn" : null}`}
+                onClick={() => {
+                  setValue(index);
+                }}
+              >
+                {val.company}
+              </button>
+            </>
+          );
+        })}
+      </div>
+      <div className="">
+        <h3 className="job-info">{title}</h3>
+        <h4 className="job-date">{dates}</h4>
+        <div className="job-desc">
+          {duties.map((description) => {
             return (
-              <div key={index}>
-                <h2>{val}</h2>
-              </div>
+              <>
+                <p key={uuidv4()}>{description}</p>
+              </>
             );
           })}
         </div>
-      </section>
-    </>
+      </div>
+    </article>
   );
 };
 export default App;
