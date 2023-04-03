5954cff053b5367d7bc08041710c10d2baf6fb3a
vishnu <vishnu981203@gmail.com>
2023-04-03 05:52:31 -0400

    Clean the folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/index.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function App() {
+const link = "https://course-api.com/react-tabs-project";
+
+const App = () => {
+  // initial state
+  const [start, setStart] = useState(true);
+  // data fetching state
+  const [getData, setGetData] = useState([]);
+
+  // fetching data using async
+  const fetchData = async () => {
+    const response = await fetch(link);
+    const data = await response.json();
+    // console.log(data);
+    setStart(false);
+    setGetData(data);
+    // console.log(getData);
+  };
+
+  // useEffect
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (start) {
+    <h2>Loading..!!</h2>;
+  }
+  // component return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <div>
+          {getData.map((val, index) => {
+            {
+              console.log(val);
+            }
+            return (
+              <div key={index}>
+                <h2>{val[1]}</h2>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
-}
-
+};
 export default App;
